d97822f1cfcef23a66725960f88b631673980135
Laurin <lau-b@users.noreply.github.com>
2015-12-09 23:53:18 +0100

    created button, started js, but its still shit.

---
+++ js/square.js
@@ -0,0 +1,9 @@
+var htmlSquare;
+var size = 8;
+
+for ( var row = 0; row < size; i++) {
+  for ( var col = 0; col < size; i++) {
+    htmlSquare += '<div class="container"></div>';
+  }
+  htmlSquare += '<br />';
+}
